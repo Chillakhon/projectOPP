7cd6b6ef2e6be20d11a5f439917820bf502b45ca
chollakhon <shoev-03@mail.ru>
2023-09-24 17:16:58 +0500

    7

--- Validate.php
+++ Validate.php
@@ -1,1 +1,74 @@
 <?php
+class Validate
+{
+    private $passed = false, $erorrs = [], $db = null;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+    public function check($source,$items = [])
+    {
+        foreach ($items as $item =>$rules)
+        {
+            foreach ($rules as $rule=>$rule_value)
+            {
+                $value = $source[$item];
+                if ($rule == 'required' && empty($value))
+                {
+                    $this->addError("{$item} is required");
+                }else if (!empty($value))
+                {
+                    switch ($rule)
+                    {
+                        case 'min':
+                            if (strlen($value) < $rule_value)
+                            {
+                                $this->addError("$item must be a minimum of $rule_value characters.");
+                            }
+                        break;
+                         case 'max':
+                            if (strlen($value) > $rule_value)
+                                {
+                                    $this->addError("$item must be a maximum $rule_value characters.");
+                                }
+                         break;
+                        case 'matches':
+                            if ($value != $source[$rule_value])
+                            {
+                                $this->addError("$rule_value must match $item");
+                            }
+                        break;
+                        case 'unique':
+                            $check = $this->db->get($rule_value,[$item, '=', $value]);
+                            if ($check->count())
+                            {
+                                $this->addError("$item already exists");
+                            }
+                        break;
+                    }
+                }
+            }
+        }
+        if (empty($this->erorrs))
+        {
+            $this->passed = true;
+        }
+        return $this;
+    }
+
+    public function addError($error)
+    {
+        $this->erorrs[]=$error;
+    }
+
+    public function errors()
+    {
+        return $this->erorrs;
+    }
+
+    public function passed()
+    {
+        return $this->passed;
+    }
+}
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 require_once "Config.php";
 require_once "Database.php";
-$id = 4;
-
+require_once "Input.php";
+require_once "Validate.php";
 
 
 //$users = Database::getInstance()->query("SELECT * FROM users WHERE email IN (?)",['Shoev']);
@@ -23,23 +23,64 @@ $GLOBALS['config'] =
             ]
     ];
 
-$users = Database::getInstance()->get('users',['password','=','4321']);
-var_dump($users->first()->email);
-
-/*$users = Database::getInstance()->get('users',['email','=','str']);
-
-echo $users->first()->email;
-
-if ($users->error())
+if (Input::exists())
 {
-    echo "error";
-}else
-{
-    foreach ($users->result() as $user)
+    $validate = new Validate();
+    $validation = $validate->check($_POST,
+        [
+                "username"=> [
+                            "required"=>true,
+                        "min"=>2,
+                        "max"=>15,
+                        "unique"=>"users"
+                    ],
+                "password"=>[
+                    "required"=>true,
+                    "min"=>3
+                ],
+                "password_again"=>[
+                    "required"=>true,
+                    "matches"=>"password"
+                ]
+        ]);
+    if ($validation->passed())
     {
-        echo $user->email . "<br>";
+        echo 'passed';
+    }else
+    {
+        foreach ($validation->errors() as $error)
+        {
+            echo $error . "<br>";
+        }
     }
-}*/
+}
+
+
+
+
+?>
+
+<form action="" method="post">
+
+    <div class="field">
+        <label for="username">Username</label>
+        <input name="username" type="text" value="<?php echo Input::get('username')?>">
+    </div>
+
+    <div class="field">
+        <label for="">Password</label>
+        <input type="text" name="password">
+    </div>
+
+    <div class="field">
+        <label for="">Password Again</label>
+        <input type="text" name="password_again">
+    </div>
+
+    <div class="field">
+        <button type="submit">Submit</button>
+    </div>
+</form>
 
 
 
